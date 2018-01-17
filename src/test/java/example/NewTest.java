d3893b0fe0b93111910eeadcb50522db948668ff
mirzavishal <vishal.mirza@nyu.edu>
2018-01-17 16:55:07 -0500

    'IC'

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,29 @@
+package example;		
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.chrome.ChromeDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;	
+	    String driverPath = "C:\\Class\\Selenium\\";
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/selenium/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		    System.out.println("launching chrome browser"); 
+		    System.setProperty("webdriver.chrome.driver", driverPath+"chromedriver.exe");
+		    driver = new ChromeDriver();
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
